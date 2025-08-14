168a76060fe8a78747c7892944aaec9b9080f5e1
João Victor de Lima Freitas <jvlf@cin.ufpe.br>
2025-08-14 10:42:24 -0300

    primeiro commit
    início de tudo. foi implementado animações no player e nos inimigos (parado, correndo e morrendo), adicionada a mecânica de tiro, de vida e de "colisão".

---
+++ projetos-python/src/config.py
@@ -0,0 +1,18 @@
+#config for main directory
+
+#* size for screen
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+#* define framerate
+FPS = 60
+
+#* define colours
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+
+#* to update animation
+ANIMATION_COOLDOWN = 100
+
+#* define game variables
+GRAVITY = 0.75
